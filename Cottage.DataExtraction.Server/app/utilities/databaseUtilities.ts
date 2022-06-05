9c6f83f88d669651003835c10625779a4b877130
Woody_FrameworkWindows <c.woodward10@gmail.com>
2022-06-05 09:58:31 -0700

    get express server up and running

---
+++ Cottage.DataExtraction.Server/app/utilities/databaseUtilities.ts
@@ -0,0 +1,24 @@
+import { Client } from 'pg';
+import { dbConfig } from '../config/dbConfig'
+
+export async function GetDataFromDatabase() {
+    const client = new Client({
+        user: dbConfig.USER,
+        password: dbConfig.PASSWORD,
+        host: dbConfig.HOST,
+        database: dbConfig.DATABASE,
+    })
+    await client.connect()
+
+    const wallsRaw = await client.query('SELECT * from walls');
+    const floorsRaw = await client.query('SELECT * from walls');
+    const plumbingFixturesRaw = await client.query('SELECT * from walls');
+
+    await client.end()
+
+    return {
+        walls: wallsRaw.rows,
+        floors: floorsRaw.rows,
+        plumbingFixtures: plumbingFixturesRaw.rows
+    };
+}
